95487418c50355d0065e2bff266fc538dce4b95c
SourDumplings <changzheng300@foxmail.com>
2019-11-24 09:22:09 +0800

    优化了依赖管理，使用了 LomBok 插件来简化 Bean 代码.

--- fitness-web-admin/src/main/java/com/zju/se/nohair/fitness/web/admin/dao/po/User.java
+++ fitness-web-admin/src/main/java/com/zju/se/nohair/fitness/web/admin/dao/po/User.java
@@ -1,7 +1,10 @@
 package com.zju.se.nohair.fitness.web.admin.dao.po;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.zju.se.nohair.fitness.commons.persistence.BaseEntity;
 import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,42 +16,13 @@ import org.springframework.stereotype.Component;
  * @date 2019/11/21 14:46
  */
 @Component
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class User extends BaseEntity implements Serializable {
+
   private String username;
+  @JsonIgnore
+  private String password;
   private String phone;
   private String email;
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  @Override
-  public String toString() {
-    final StringBuilder sb = new StringBuilder("User{");
-    sb.append("username='").append(username).append('\'');
-    sb.append(", phone='").append(phone).append('\'');
-    sb.append(", email='").append(email).append('\'');
-    sb.append('}');
-    return sb.toString();
-  }
 }
