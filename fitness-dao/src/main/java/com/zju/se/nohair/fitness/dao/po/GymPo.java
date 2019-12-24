e8d3d89ff7a52bd973d65b97a2c4492f9f82ace7
SourDumplings <changzheng300@foxmail.com>
2019-12-24 18:47:33 +0800

    修改了 gym 表的拼错的 business.

--- fitness-dao/src/main/java/com/zju/se/nohair/fitness/dao/po/GymPo.java
+++ fitness-dao/src/main/java/com/zju/se/nohair/fitness/dao/po/GymPo.java
@@ -4,73 +4,73 @@ import java.util.Date;
 
 public class GymPo {
 
-    private Integer id;
+  private Integer id;
 
-    private String name;
+  private String name;
 
-    private String address;
+  private String address;
 
-    private String content;
+  private String content;
 
-    private Integer bussinessId;
+  private Integer businessId;
 
-    private Integer picGroupId;
+  private Integer picGroupId;
 
-    private Date createdTime;
+  private Date createdTime;
 
-    public Integer getId() {
-        return id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
+  public void setName(String name) {
+    this.name = name == null ? null : name.trim();
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public String getAddress() {
+    return address;
+  }
 
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
+  public void setAddress(String address) {
+    this.address = address == null ? null : address.trim();
+  }
 
-    public String getContent() {
-        return content;
-    }
+  public String getContent() {
+    return content;
+  }
 
-    public void setContent(String content) {
-        this.content = content == null ? null : content.trim();
-    }
+  public void setContent(String content) {
+    this.content = content == null ? null : content.trim();
+  }
 
-    public Integer getBussinessId() {
-        return bussinessId;
-    }
+  public Integer getBusinessId() {
+    return businessId;
+  }
 
-    public void setBussinessId(Integer bussinessId) {
-        this.bussinessId = bussinessId;
-    }
+  public void setBusinessId(Integer businessId) {
+    this.businessId = businessId;
+  }
 
-    public Integer getPicGroupId() {
-        return picGroupId;
-    }
+  public Integer getPicGroupId() {
+    return picGroupId;
+  }
 
-    public void setPicGroupId(Integer picGroupId) {
-        this.picGroupId = picGroupId;
-    }
+  public void setPicGroupId(Integer picGroupId) {
+    this.picGroupId = picGroupId;
+  }
 
-    public Date getCreatedTime() {
-        return createdTime;
-    }
+  public Date getCreatedTime() {
+    return createdTime;
+  }
 
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
+  public void setCreatedTime(Date createdTime) {
+    this.createdTime = createdTime;
+  }
 }
