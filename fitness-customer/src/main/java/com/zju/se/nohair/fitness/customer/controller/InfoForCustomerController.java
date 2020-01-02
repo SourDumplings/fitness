b60e5c0722c538669051b41cd10140871afdc40a
Willowies <z654714845@126.com>
2020-01-02 22:29:35 +0800

    测试linux环境下的虚拟路径和文件存取

--- fitness-customer/src/main/java/com/zju/se/nohair/fitness/customer/controller/InfoForCustomerController.java
+++ fitness-customer/src/main/java/com/zju/se/nohair/fitness/customer/controller/InfoForCustomerController.java
@@ -1,11 +1,17 @@
 package com.zju.se.nohair.fitness.customer.controller;
 
 import com.zju.se.nohair.fitness.commons.dto.BaseResult;
+import com.zju.se.nohair.fitness.customer.dto.CommentCourseDto;
 import com.zju.se.nohair.fitness.customer.service.CoachInCustomerService;
 import com.zju.se.nohair.fitness.customer.service.CustomerService;
 import com.zju.se.nohair.fitness.customer.service.GymInCustomerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +20,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 客户端需要的信息 包括健身房、教练等信息
@@ -39,6 +47,56 @@ public class InfoForCustomerController {
   private GymInCustomerService gymService;
   @Autowired
   private CustomerService customerService;
+
+  @ApiOperation(value = "接收图片和参数的demo", httpMethod = "POST")
+  @RequestMapping(value = "pic", method = RequestMethod.POST)
+  @ResponseBody
+  public ResponseEntity<Object> getPic(@RequestParam("file") MultipartFile file, CommentCourseDto commentCourseDto) {
+
+    try {
+      savePic(file.getInputStream(),"1.jpg");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    BaseResult baseResult = BaseResult.success("success");
+    baseResult.setData(commentCourseDto);
+    return new ResponseEntity<>(baseResult.getData(),HttpStatus.OK);
+  }
+
+  private void savePic(InputStream inputStream, String fileName) {
+    OutputStream os = null;
+    try {
+      String path = "/pic/customer/";
+      // 2、保存到临时文件
+      // 1K的数据缓冲
+      byte[] bs = new byte[1024];
+      // 读取到的数据长度
+      int len;
+      // 输出的文件流保存到本地文件
+      File tempFile = new File(path);
+      if (!tempFile.exists()) {
+        tempFile.mkdirs();
+      }
+      os = new FileOutputStream(tempFile.getPath() + File.separator + fileName);
+      // 开始读取
+      while ((len = inputStream.read(bs)) != -1) {
+        os.write(bs, 0, len);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      // 完毕，关闭所有链接
+      try {
+        os.close();
+        inputStream.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
   @ApiOperation(value = "查询教练详细信息", httpMethod = "GET")
   @RequestMapping(value = "details/coach/{coachId}", method = RequestMethod.GET)
---
+++ fitness-main-app/src/main/java/com/zju/se/nohair/fitness/main/app/config/FilePathConfig.java
@@ -0,0 +1,32 @@
+package com.zju.se.nohair.fitness.main.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 文件路径与虚拟路径
+ *
+ * @author Wang Haowen
+ * @version 1.0.0
+ * @projectName fitness
+ * @date 2020/1/2 21:37
+ */
+@Configuration
+public class FilePathConfig implements WebMvcConfigurer {
+
+  /**
+   * 虚拟路径配置
+   * @param registry
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    //linux下的配置
+    registry.addResourceHandler("/pic/**").addResourceLocations("file:" + "/pic/");
+    //windows下的配置
+    //registry.addResourceHandler("/pic/**").addResourceLocations("file:" + "D:/data/");
+    WebMvcConfigurer.super.addResourceHandlers(registry);
+  }
+}
