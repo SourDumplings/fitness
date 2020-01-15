a399f320470a2aa0d024c87abea71515a53a557d
summer199606 <748807363@qq.com>
2020-01-15 11:48:26 +0800

    查看我发布的私教课列表

---
+++ fitness-coach/src/main/java/com/zju/se/nohair/fitness/coach/dto/PrivateCourseListItemDto.java
@@ -0,0 +1,29 @@
+package com.zju.se.nohair.fitness.coach.dto;
+
+import java.util.Date;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+/**
+ * 教练查看自己发布的私教课列表 dto.
+ * @author Miss.Hu
+ * @version 1.0.0
+ * @projectName fitness
+ * @date 2020-01-15
+ */
+@Component
+@Data
+public class PrivateCourseListItemDto {
+
+  private Integer id;
+
+  private String name;
+
+  private Integer coachId;
+
+  private Date courseDate;
+
+  //private Integer timeSlotId;
+
+  private Integer status;
+}
