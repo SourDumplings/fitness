8243937268e5b42d137edeb6062498a7a7a8727b
SourDumplings <changzheng300@foxmail.com>
2020-02-13 20:54:20 +0800

    代码维护：修复了 Po 和 表不一致的问题.

--- fitness-dao/src/main/java/com/zju/se/nohair/fitness/dao/po/ResponsesPrivatePo.java
+++ fitness-dao/src/main/java/com/zju/se/nohair/fitness/dao/po/ResponsesPrivatePo.java
@@ -4,26 +4,6 @@ import java.math.BigDecimal;
 
 public class ResponsesPrivatePo extends ResponsesPrivatePoKey {
 
-  private String name;
-
-  private String address;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
   private BigDecimal price;
 
   private Integer status;
