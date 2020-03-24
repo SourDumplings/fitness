24c695c7aa9d17250d9b1aa9fa49004bad1bfdfc
changzheng <changzheng300@foxmail.com>
2020-03-24 15:00:58 +0800

    后台端用户管理：实现了商家用户的批量删除接口

--- fitness-web-admin/src/main/java/com/zju/se/nohair/fitness/web/admin/controller/AdminBusinessUserController.java
+++ fitness-web-admin/src/main/java/com/zju/se/nohair/fitness/web/admin/controller/AdminBusinessUserController.java
@@ -5,12 +5,14 @@ import com.zju.se.nohair.fitness.web.admin.dto.AdminCreateBusinessUserDto;
 import com.zju.se.nohair.fitness.web.admin.service.AdminBusinessUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,25 +31,31 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("admin/user/business")
 @Api
-public class AdminBusinessUserController {
+public class AdminBusinessUserController
+{
 
   private static Logger logger = LoggerFactory.getLogger(AdminBusinessUserController.class);
 
   private AdminBusinessUserService adminBusinessUserService;
 
   @Autowired
-  public void setAdminBusinessUserService(AdminBusinessUserService adminBusinessUserService) {
+  public void setAdminBusinessUserService(AdminBusinessUserService adminBusinessUserService)
+  {
     this.adminBusinessUserService = adminBusinessUserService;
   }
 
   @ApiOperation(value = "商家用户列表；审核状态（0代表未审批，1代表已审批成功，2代表审批未通过）", httpMethod = "GET")
   @RequestMapping(value = "", method = RequestMethod.GET)
   @ResponseBody
-  public ResponseEntity<Object> listAll() {
+  public ResponseEntity<Object> listAll()
+  {
     BaseResult baseResult = adminBusinessUserService.listAll();
-    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS) {
+    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS)
+    {
       return new ResponseEntity<>(baseResult.getData(), HttpStatus.OK);
-    } else {
+    }
+    else
+    {
       return new ResponseEntity<>(baseResult.getMessage(),
           HttpStatus.valueOf(baseResult.getStatus()));
     }
@@ -57,11 +65,15 @@ public class AdminBusinessUserController {
   @RequestMapping(value = "{businessId}", method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<Object> getDetailById(
-      @PathVariable(value = "businessId") Integer businessId) {
+      @PathVariable(value = "businessId") Integer businessId)
+  {
     BaseResult baseResult = adminBusinessUserService.getDetailById(businessId);
-    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS) {
+    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS)
+    {
       return new ResponseEntity<>(baseResult.getData(), HttpStatus.OK);
-    } else {
+    }
+    else
+    {
       return new ResponseEntity<>(baseResult.getMessage(),
           HttpStatus.valueOf(baseResult.getStatus()));
     }
@@ -73,12 +85,16 @@ public class AdminBusinessUserController {
   public ResponseEntity<Object> saveItem(
       AdminCreateBusinessUserDto adminCreateBusinessUserDto,
       @RequestParam("profilePic") MultipartFile profilePic,
-      @RequestParam("certificationPic") MultipartFile certificationPic) {
+      @RequestParam("certificationPic") MultipartFile certificationPic)
+  {
     BaseResult baseResult = adminBusinessUserService.saveItem(adminCreateBusinessUserDto,
         profilePic, certificationPic);
-    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS) {
+    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS)
+    {
       return new ResponseEntity<>(baseResult.getMessage(), HttpStatus.CREATED);
-    } else {
+    }
+    else
+    {
       return new ResponseEntity<>(baseResult.getMessage(),
           HttpStatus.valueOf(baseResult.getStatus()));
     }
@@ -88,11 +104,33 @@ public class AdminBusinessUserController {
   @RequestMapping(value = "{businessId}", method = RequestMethod.DELETE)
   @ResponseBody
   public ResponseEntity<Object> deleteItem(
-      @PathVariable(value = "businessId") Integer businessId) {
+      @PathVariable(value = "businessId") Integer businessId)
+  {
     BaseResult baseResult = adminBusinessUserService.deleteItem(businessId);
-    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS) {
+    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS)
+    {
       return new ResponseEntity<>(baseResult.getData(), HttpStatus.NO_CONTENT);
-    } else {
+    }
+    else
+    {
+      return new ResponseEntity<>(baseResult.getMessage(),
+          HttpStatus.valueOf(baseResult.getStatus()));
+    }
+  }
+
+  @ApiOperation(value = "批量删除商家用户；参数输入格式示例：[id1,id2,id3]", httpMethod = "DELETE")
+  @RequestMapping(value = "", method = RequestMethod.DELETE)
+  @ResponseBody
+  public ResponseEntity<Object> deleteMulti(
+      @RequestBody List<Integer> businessIds)
+  {
+    BaseResult baseResult = adminBusinessUserService.deleteMulti(businessIds);
+    if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS)
+    {
+      return new ResponseEntity<>(baseResult.getData(), HttpStatus.NO_CONTENT);
+    }
+    else
+    {
       return new ResponseEntity<>(baseResult.getMessage(),
           HttpStatus.valueOf(baseResult.getStatus()));
     }
