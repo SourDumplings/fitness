1a0699ed96b64bdd2db8abae04a4625c60a8f101
SourDumplings <changzheng300@foxmail.com>
2020-04-03 22:07:59 +0800

    Revert "商家端：去掉了注册接口的图片项."
    This reverts commit ca33593a

--- fitness-business/src/main/java/com/zju/se/nohair/fitness/business/controller/UserInfoController.java
+++ fitness-business/src/main/java/com/zju/se/nohair/fitness/business/controller/UserInfoController.java
@@ -47,8 +47,11 @@ public class UserInfoController {
   @RequestMapping(value = "", method = RequestMethod.POST)
   @ResponseBody
   public ResponseEntity<Object> createPublicCourse(
-      CreateBusinessUserDto createBusinessUserDto) {
-    BaseResult baseResult = userInfoService.createBusinessUser(createBusinessUserDto);
+      CreateBusinessUserDto createBusinessUserDto,
+      @RequestParam("profilePic") MultipartFile profilePic,
+      @RequestParam("certificationPic") MultipartFile certificationPic) {
+    BaseResult baseResult = userInfoService.createBusinessUser(createBusinessUserDto,
+        profilePic, certificationPic);
     if (baseResult.getStatus() == BaseResult.STATUS_SUCCESS) {
       return new ResponseEntity<>(baseResult.getMessage(), HttpStatus.CREATED);
     } else {
